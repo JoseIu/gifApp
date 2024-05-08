79376e44ed5b9fe1d9ca4b0e8d7a2fdd9dc0189c
Joselu <joseluissgz97@gmail.com>
2024-05-08 23:03:12 +0200

    add: GifCard

---
+++ src/components/GifCard/GifCard.tsx
@@ -0,0 +1,20 @@
+import { Gif } from '../interfaces/gif.interface';
+import style from './GifCard.module.scss';
+
+interface GifCardProps {
+  gif: Gif;
+}
+const GifCard = ({ gif }: GifCardProps) => {
+  return (
+    <article className={style.gif}>
+      <img className={style.gif__img} src={gif.url} alt={gif.alt_text} />
+
+      <div className={style.gif__info}>
+        <h3 className={style.gif__title}>{gif.title}</h3>
+        <span>User: {gif.user ? gif.user.username : 'Unknow'} </span>
+      </div>
+    </article>
+  );
+};
+
+export default GifCard;
