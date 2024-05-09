d514142839ea7eeb8cbddd946941241edce689f5
Joselu <joseluissgz97@gmail.com>
2024-05-09 14:21:25 +0200

    add: routes for GiffApp and GifDetailes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import GifApp from './GifApp.tsx';
+import GifDetailes from './components/GifDetails/GifDetailes.tsx';
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GifApp />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index path="gifs" element={<GifApp />} />
+      <Route path="gifs/:id" element={<GifDetailes />} />
+      <Route path="*" element={<Navigate to={'gifs'} />} />
+    </Routes>
+  </BrowserRouter>
+  // <React.StrictMode>
+  // </React.StrictMode>
 );
