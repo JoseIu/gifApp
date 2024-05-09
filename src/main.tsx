a29271fc9b5ce8de1afb215345bf76fddcd65c4c
Joselu <joseluissgz97@gmail.com>
2024-05-09 21:00:11 +0200

    add: providing GifsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import GifApp from './GifApp.tsx';
 import GifDetailes from './components/GifDetails/GifDetailes.tsx';
+import { GifsProvider } from './context/gifsProvider.tsx';
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route index path="gifs" element={<GifApp />} />
-      <Route path="gifs/:id" element={<GifDetailes />} />
-      <Route path="*" element={<Navigate to={'gifs'} />} />
-    </Routes>
-  </BrowserRouter>
+  <GifsProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="gifs" element={<GifApp />} />
+        <Route path="gifs/:id" element={<GifDetailes />} />
+        <Route path="*" element={<Navigate to={'gifs'} />} />
+      </Routes>
+    </BrowserRouter>
+  </GifsProvider>
   // <React.StrictMode>
   // </React.StrictMode>
 );
