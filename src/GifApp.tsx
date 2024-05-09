c87758508b65746142be2e2dab4b56e5555beee3
Joselu <joseluissgz97@gmail.com>
2024-05-09 21:00:01 +0200

    refacot: mocving useSearchHistory to hooks folder

--- src/GifApp.tsx
+++ src/GifApp.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
 import style from './GifApp.module.scss';
 import AddCategory from './components/AddCategory/AddCategory';
 import GisfsList from './components/GifsList/GisfsList';
+import { useSearchHistory } from './hooks/useSearchHistory';
 
 const GifApp = () => {
-  const { searchTerm, history, setSearchTerm } = useSearchHistory();
+  const { history, setSearchTerm } = useSearchHistory();
 
   const onAddCategory = (value: string) => {
     if (history.includes(value)) return;
@@ -15,31 +15,9 @@ const GifApp = () => {
     <main className={`${style.gif} wrapper`}>
       <h1 className={style.gif__title}>GifApp</h1>
       <AddCategory onNewCategory={(value) => onAddCategory(value)} />
-
-      {searchTerm && <GisfsList searchTerm={searchTerm} />}
+      <GisfsList />
     </main>
   );
-};
-
-interface SearchHistory {
-  history: string[];
-  searchTerm: string;
-}
-
-const useSearchHistory = () => {
-  const [searchInfo, setSearchInfo] = useState<SearchHistory>({
-    history: [],
-    searchTerm: '',
-  });
-
-  const setSearchTerm = (searchTerm: string) =>
-    setSearchInfo({ history: [searchTerm, ...searchInfo.history], searchTerm });
-
-  return {
-    history: searchInfo.history,
-    searchTerm: searchInfo.searchTerm,
-    setSearchTerm,
-  };
 };
 
 export default GifApp;
