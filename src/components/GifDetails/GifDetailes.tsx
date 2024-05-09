cf8d97d2005eda259b6d06a250b872b31e5adbd6
Joselu <joseluissgz97@gmail.com>
2024-05-09 21:01:25 +0200

    add: finding and showing gif deatils

--- src/components/GifDetails/GifDetailes.tsx
+++ src/components/GifDetails/GifDetailes.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useGifs from '../../hooks/useGifs';
+import { Gif } from '../../interfaces/gif.interface';
+
 const GifDetailes = () => {
-  return <div>HOLAAAAA</div>;
+  const { id } = useParams();
+  const [gifDetails, setGifDetails] = useState<Gif>();
+  const { gifs } = useGifs();
+
+  useEffect(() => {
+    const gif = gifs.find((gif) => gif.id === id);
+    if (!gif) return;
+    setGifDetails(gif);
+  }, [id, gifs]);
+  return (
+    <section>
+      <span>GOla</span>
+      <h2> {gifDetails?.title} </h2>
+      <img src={gifDetails?.url} alt="" />
+    </section>
+  );
 };
 
 export default GifDetailes;
