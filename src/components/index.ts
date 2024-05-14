49f3f74b8431749218b7b1c2a16b0e6ed201fcca
Joselu <joseluissgz97@gmail.com>
2024-05-14 19:16:36 +0200

    feat: add Barril to export all componentes index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as AddCategory } from './AddCategory/AddCategory';
+export { default as GifCard } from './GifCard/GifCard';
+export { default as GifDetails } from './GifDetails/GifDetailes';
+export { default as GisfsList } from './GifsList/GisfsList';
